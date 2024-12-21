fe26817f5f0aa7b7c49b1e8b6faaa4929e4f6548
alfa0805 <asadbekarslonbekov53@gmail.com>
2024-12-21 14:49:33 +0500

    salom

---
+++ main.js
@@ -0,0 +1,41 @@
+let phonenumber =document.querySelector("#phone_number")
+let password =document.querySelector("#password");
+let eye =document.querySelector('.eye');
+let btn =document.querySelector("#btn");
+
+btn.addEventListener('click' , function(e){
+    e.preventDefault();
+    let data = new FormData()
+    data.append('phone_number',phonenumber?.value)
+    data.append('password' , password?.value)
+    fetch('https://realauto.limsa.uz/api/auth/signin',{ 
+        method: 'POST',
+        body: data
+    }).then(res=>{
+        if(res.ok){
+            Swal.fire({
+                title: "Good job!",
+                text: "You clicked the button!",
+                icon: "success"
+              });
+        } else{
+            Swal.fire({
+                title: "Error",
+                text: "You clicked the button!",
+                icon: "error"
+              });
+        }
+    }).catch(err=>{
+        alert('Error')
+    });
+});
+
+
+eye.addEventListener('click', function(e){
+    if (password.type == "password"){
+        password.type = "text"
+    } else{
+        password.type = "password"
+    }
+
+})
